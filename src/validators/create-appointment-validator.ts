c420201b1371582c469bd62d2fb2339d5be364d7
Diego Luna <dpl@cesar.school>
2024-07-08 02:11:28 -0300

    feat: add birthDay to the create-appointment-validator

--- src/validators/create-appointment-validator.ts
+++ src/validators/create-appointment-validator.ts
@@ -1,3 +1,4 @@
+import { isBefore, parseISO } from 'date-fns'
 import { FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -9,6 +10,34 @@ export const createAppointmentValidator = z.object({
     })
     .trim()
     .min(1, { message: 'Name must not be empty' }),
+  birthDay: z
+    .string()
+    .refine(
+      (dateStr) => {
+        const regex = /^\d{4}-\d{2}-\d{2}$/
+        return regex.test(dateStr)
+      },
+      {
+        message: 'Invalid date format. Expected format: YYYY-MM-DD',
+      },
+    )
+    .refine(
+      (dateStr) => {
+        const date = parseISO(dateStr)
+
+        return isBefore(date, new Date())
+      },
+      {
+        message: 'The date must be in the past.',
+      },
+    )
+    .transform((dateStr) => {
+      const date = parseISO(dateStr)
+
+      return new Date(
+        Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()),
+      )
+    }),
 })
 
 export type CreateAppointmentInput = z.infer<typeof createAppointmentValidator>
