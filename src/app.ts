a26c00914d9a320b57287140eef3c325d6c2dca7
Diego Luna <dpl@cesar.school>
2024-07-08 00:46:10 -0300

    feat: setup logging

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import fastifyHelmet from '@fastify/helmet'
 import fastify from 'fastify'
 
-export const app = fastify()
+export const app = fastify({ logger: true })
 
 app.register(fastifyHelmet)
