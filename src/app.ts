ed3de0722dd4f0e9ad6f7cc2c28f471e7b5b8478
Diego Luna <dpl@cesar.school>
2024-07-08 00:48:45 -0300

    feat: setup custom error handler

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,11 @@ import fastify from 'fastify'
 export const app = fastify({ logger: true })
 
 app.register(fastifyHelmet)
+
+app.setErrorHandler((error, _, reply) => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  reply.status(500).send({ message: 'Internal server error' })
+})
