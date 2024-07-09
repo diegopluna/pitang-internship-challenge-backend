6fbd6f3402bf138735d29bc043261fc2e0b0d99c
Diego Luna <dpl@cesar.school>
2024-07-08 21:02:41 -0300

    feat: add list-appointments route

--- src/http/controllers/appointments/routes.ts
+++ src/http/controllers/appointments/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
+import { list } from './list'
 
 export async function appointmentsRoutes(app: FastifyInstance) {
   app.post('/', create)
+  app.get('/', list)
 }
