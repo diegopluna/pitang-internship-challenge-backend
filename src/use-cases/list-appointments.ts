b8548ed3fd8f5ba2fc4d45765a024de1fc90c51c
Diego Luna <dpl@cesar.school>
2024-07-11 16:39:34 -0300

    feat: simplify list-appointments use-case

--- src/use-cases/list-appointments.ts
+++ src/use-cases/list-appointments.ts
@@ -2,11 +2,7 @@ import { Appointment } from '@prisma/client'
 import { AppointmentsRepository } from '../repositories/appointments-repository'
 
 interface ListAppointmentsUseCaseResponse {
-  appointments: {
-    [date: string]: {
-      [hour: string]: Appointment[]
-    }
-  }
+  appointments: Appointment[]
 }
 
 export class ListAppointmentsUseCase {
@@ -15,24 +11,6 @@ export class ListAppointmentsUseCase {
   async execute(): Promise<ListAppointmentsUseCaseResponse> {
     const appointments = await this.appointmentsRepository.findAll()
 
-    const groupedAppointments: ListAppointmentsUseCaseResponse['appointments'] =
-      {}
-
-    for (const appointment of appointments) {
-      const date = appointment.appointmentDate.toISOString().split('T')[0]
-      const hour = `${appointment.appointmentDate.getUTCHours().toString().padStart(2, '0')}:${appointment.appointmentDate.getUTCMinutes().toString().padStart(2, '0')}`
-
-      if (!groupedAppointments[date]) {
-        groupedAppointments[date] = {}
-      }
-
-      if (!groupedAppointments[date][hour]) {
-        groupedAppointments[date][hour] = []
-      }
-
-      groupedAppointments[date][hour].push(appointment)
-    }
-
-    return { appointments: groupedAppointments }
+    return { appointments }
   }
 }
