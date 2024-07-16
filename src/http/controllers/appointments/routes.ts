22298f5545b4920d2d0bff77feb7e202f955fc8d
Diego Peter <dpl@cesar.school>
2024-07-15 21:30:38 -0300

    feat: add update route

--- src/http/controllers/appointments/routes.ts
+++ src/http/controllers/appointments/routes.ts
@@ -3,10 +3,12 @@ import { create } from './create'
 import { list } from './list'
 import { getById } from './get-by-id'
 import { toggle } from './toggle'
+import { update } from './update'
 
 export async function appointmentsRoutes(app: FastifyInstance) {
   app.post('/', create)
   app.get('/', list)
   app.get('/:id', getById)
+  app.put('/:id', update)
   app.patch('/:id/toggle-vaccinated', toggle)
 }
