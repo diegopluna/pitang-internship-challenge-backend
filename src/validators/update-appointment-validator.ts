dc4262a24683bff084c29ed354ff53134cb87685
Diego Peter <dpl@cesar.school>
2024-07-19 13:37:03 -0300

    feat: improve update appointment validator logic

--- src/validators/update-appointment-validator.ts
+++ src/validators/update-appointment-validator.ts
@@ -1,70 +1,8 @@
-import {
-  isIsoDateFormat,
-  isIsoDateStringBeforeCurrentDate,
-  isIsoDateStringValid,
-  isTimestampValid,
-  newUTCDateFromIsoDateString,
-  newUTCDateFromTimestamp,
-} from '@/utils/date-utils'
 import { FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { createAppointmentValidator } from './create-appointment-validator'
 
-export const updateAppointmentValidator = z.object({
-  name: z
-    .string({
-      required_error: 'Nome é obrigatório',
-      invalid_type_error: 'Nome deve ser uma string',
-    })
-    .trim()
-    .min(1, { message: 'Nome não pode ser vazio' }),
-  birthDay: z
-    .string()
-    .refine(
-      (dateStr) => {
-        return isIsoDateFormat(dateStr)
-      },
-      {
-        message:
-          'Formato de data de aniversário inválido. Formato esperado: YYYY-MM-DD',
-      },
-    )
-    .refine(
-      (dateStr) => {
-        return isIsoDateStringValid(dateStr)
-      },
-      {
-        message: 'Data de aniversário inválida. Formato esperado: YYYY-MM-DD',
-      },
-    )
-    .refine(
-      (dateStr) => {
-        return isIsoDateStringBeforeCurrentDate(dateStr)
-      },
-      {
-        message: 'A data de aniversário deve ser anterior à data atual.',
-      },
-    )
-    .transform((dateStr) => {
-      return newUTCDateFromIsoDateString(dateStr)
-    }),
-  appointmentDate: z
-    .number({
-      required_error: 'Data de agendamento é obrigatória',
-      invalid_type_error:
-        'Data de agendamento deve ser um número no formato Unix timestamp',
-    })
-    .refine(
-      (timestamp) => {
-        return isTimestampValid(timestamp)
-      },
-      {
-        message:
-          'Data de agendamento inválida. Formato esperado: Unix timestamp em milissegundos',
-      },
-    )
-    .transform((timestamp) => {
-      return newUTCDateFromTimestamp(timestamp)
-    }),
+export const updateAppointmentValidator = createAppointmentValidator.extend({
   vaccinationComplete: z.boolean({
     required_error: 'VaccinationComplete é obrigatório',
     invalid_type_error: 'VaccinationComplete deve ser um booleano',
